1bc02d0d110744a86a42aaf0451612db1ae0748e
KulbirJ <jaglan.kulvir@gmail.com>
2015-12-20 18:42:28 -0600

    Tab View Funcationality added

--- app/src/main/java/com/example/ebuspass/ebuspassapp/MainActivity.java
+++ app/src/main/java/com/example/ebuspass/ebuspassapp/MainActivity.java
@@ -15,28 +15,50 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    Button ButtonLogout;
+    Button ButtonLogin;
     EditText FirstName, UserName, LastName, Email;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-
+        setContentView(R.layout.login);
+/*
         FirstName = (EditText) findViewById(R.id.First_Name);
         LastName = (EditText)findViewById((R.id.Last_Name));
         Email = (EditText)findViewById(R.id.Email);
         UserName = (EditText)findViewById(R.id.Username);
-
-        ButtonLogout = (Button) findViewById(R.id.ButtonLogout);
-        ButtonLogout.setOnClickListener(this);
+*/
+        ButtonLogin = (Button) findViewById(R.id.ButtonLogin);
+        ButtonLogin.setOnClickListener(this);
 
     }
+   /* @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
 
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.profile) {
+            startActivity(new Intent(this, Profile.class));
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+*/
     @Override
     public void onClick(View v){
         switch (v.getId()){
-            case R.id.ButtonLogout:
-                startActivity(new Intent(this, LoginActivity.class));
+            case R.id.ButtonLogin:
+                startActivity(new Intent(this, LoginMain.class));
                 break;
         }
     }
---
+++ app/src/main/java/com/example/ebuspass/ebuspassapp/Profile.java
@@ -0,0 +1,19 @@
+package com.example.ebuspass.ebuspassapp;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Kulbir on 2015-12-19.
+ */
+public class Profile extends AppCompatActivity {
+
+    @Override
+    public  void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profile);
+
+    }
+}
