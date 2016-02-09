91ecc5a1e7ad4d4b8739f0556815c1287ccb3d7e
KulbirJ <jaglan.kulvir@gmail.com>
2016-02-09 13:18:46 -0600

    HCE Services update

--- app/src/main/java/com/example/ebuspass/ebuspassapp/app/AppConfig.java
+++ app/src/main/java/com/example/ebuspass/ebuspassapp/app/AppConfig.java
@@ -4,9 +4,9 @@ public class AppConfig {
 
 
 	//public static String URL_LOGIN = "http://142.3.121.185/android_login_api/login.php";
-	public static String URL_LOGIN = "http://54.152.178.31/android_login_api/login.php";
+	public static String URL_LOGIN = "http://ebuspass.com/hooks/android_login_api/login.php";
 
 	// Server user register url
-	public static String URL_REGISTER = "http://54.152.178.31/android_login_api/register.php";
+	public static String URL_REGISTER = "http://ebuspass.com/hooks/android_login_api/register.php";
 //54.152.178.31
 }
